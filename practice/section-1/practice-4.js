58234af4ecb2dede9706288263d0092aa054f5dd
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-06-25 17:03:37 +0800

    implement practice4

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,12 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  const resultArray = [];
+  collectionA.map(elementA => elementA.key)
+      .forEach(elementA => {
+        if (objectB.value.flat().includes(elementA)) {
+          resultArray.push(elementA);
+        }
+      });
+  return resultArray;
 }
