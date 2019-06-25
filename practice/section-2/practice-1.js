3b2aa6340acb6659481b54524fd8ca6ee6a3dd76
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-06-25 17:14:40 +0800

    implement practice1 of section2

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const resultArray = [];
+  collection.forEach(element => {
+     const findObj = resultArray.find(item => item.key === element);
+     if (findObj) {
+       findObj.count += 1;
+     } else {
+       resultArray.push({ key: element, count: 1});
+     }
+  });
+  return resultArray;
 }
