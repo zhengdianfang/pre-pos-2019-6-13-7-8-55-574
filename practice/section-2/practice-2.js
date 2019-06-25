b92807d24e734e924ab5492c51bbf0d2a259772a
Dianfang Zheng <dfgzheng@thoughworks.com>
2019-06-25 18:05:28 +0800

    refactor practice2 and implement practice3 of section2

--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,28 @@
 'use strict';
 
+function parseElement(element) {
+  const regax = /[a-z0-9]+/g;
+  const matchRes = element.match(regax);
+  if (matchRes.length === 2) {
+    return { key: matchRes[0], count: parseInt(matchRes[1])}
+  }
+  return matchRes[0];
+}
+
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const resultArray = [];
+  collection.forEach(element => {
+     const parsedItem = parseElement(element);
+     if (parsedItem instanceof Object) {
+       resultArray.push(parsedItem);
+     } else {
+        const findObj = resultArray.find(item => item.key === element);
+        if (findObj) {
+          findObj.count += 1;
+        } else {
+          resultArray.push({ key: element, count: 1});
+        }
+     }
+  });
+  return resultArray;
 }
--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,24 @@
 'use strict';
 
+function parseElement(element) {
+  const regax = /[a-z0-9]+/g;
+  const matchRes = element.match(regax);
+  if (matchRes.length === 2) {
+    return { name: matchRes[0], summary: parseInt(matchRes[1])}
+  }
+  return { name : matchRes[0], summary: 1 };
+}
+
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const resultArray = [];
+  collection.forEach(element => {
+      const parsedItem = parseElement(element);
+      const findObj = resultArray.find(item => item.name === parsedItem.name);
+      if (findObj) {
+        findObj.summary = findObj.summary + parsedItem.summary;
+      } else {
+        resultArray.push(parsedItem);
+      }
+  });
+  return resultArray;
 }
